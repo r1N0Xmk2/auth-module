4973c04feba0190190d5bac32d8070e3fb819473
r1N0Xmk2 <rene7297152@gmail.com>
2016-07-04 09:58:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,133 @@
+#使用方法
+建立`auth-model.js`文件
+```javascript
+var auth = require('auth-module');
+//初始化 auth-model
+var authModel = auth.init({
+    agentName: 'test', //auth模块agentName
+    agentKey: 'test', //auth模块agentKey
+    authAddr: 'http://localhost:4003', //auth-node模块地址
+    refresh: 5 //重新获取token时间间隔 默认值：5 ,单位小时
+});
+module.exports = authModel;
+```
+在`app.js`中引入之前建立的`auth-model.js`文件
+```
+var auth = require('path/to/auth-model.js');
+```
+
+router中的例子
+```javascript
+router.post('/getToken', function (req, res, next) {
+  // Promise方法
+  authModel.getTokenPromise(req.body).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.getToken(req.body, function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.get('/updateToken', function (req, res, next) {
+  // Promise方法
+  authModel.updateTokenPromise().then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.updateToken(function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.post('/emailRegister', function (req, res, next) {
+  // Promise方法
+  authModel.emailRegisterPromise(req.body).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.emailRegister(req.body, function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.post('/forgetPassword', function (req, res, next) {
+  // Promise方法
+  authModel.forgetPasswordPromise(req.body).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.forgetPassword(req.body, function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.post('/getUserByAccount', function (req, res, next) {
+  // Promise方法
+  authModel.getUserByAccountPromise(req.body).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.getUserByAccount(req.body, function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.get('/getUserByOpenId/:openId', function (req, res, next) {
+  // Promise方法
+  authModel.getUserByOpenIdPromise(_.merge(req.body, req.query, req.params)).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.getUserByOpenId(_.merge(req.body, req.query, req.params), function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+router.post('/authMD5', function (req, res, next) {
+  // Promise方法
+  authModel.authMD5Promise(req.body).then(function (ret) {
+    res.send(200, ret);
+  }, function (err){
+    res.send(200, err);
+  })
+  //callback方法
+  //authModel.authMD5(req.body, function (err, ret) {
+    //if (err)  {
+      //res.send(200, err)
+    //} else {
+      //res.send(200, ret)
+    //}
+  //});
+});
+```
---
+++ index.js
@@ -0,0 +1,199 @@
+var util = require('util')
+var _ = require('lodash');
+var request = require('request');
+var async = require('async');
+var Promise = require('promise');
+// callback to promise
+var promisify = function promisify(fn, receiver) {
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return new Promise(function (resolve, reject) {
+      fn.apply(receiver, [].concat(args, [function (err, res) {
+        return err ? reject(err) : resolve(res);
+      }]));
+    });
+  };
+};
+
+function init (options) {
+    var model = {};
+    // Defined Auth-node Functions to Create
+    var ownedFuns = [
+        {
+            name: 'emailRegister',
+            url: '/emailRegister',
+            method: 'POST',
+            token: true
+        }, {
+            name: 'forgetPassword',
+            url: '/forgetPassword',
+            method: 'POST',
+            token: true
+        }, {
+            name: 'getUserByAccount',
+            url: '/getUserByAccount',
+            method: 'POST',
+            token: true
+        }, {
+            name: 'getUserByOpenId',
+            url: '/getUserByOpenId/:openId',
+            method: 'GET',
+            token: true
+        }, {
+            name: 'authMD5',
+            url: '/authMD5',
+            method: 'POST',
+            token: true
+        }
+    ];
+    // init necessary fields
+    var necessaryFields = ['agentName', 'agentKey', 'authAddr'];
+    options.refresh = options.refresh || 5;
+    if (necessaryFields.every(function (field) {
+        return _.has(options, field);
+    })) {
+        model.options = options;
+        model.getToken = function (params, callback) {
+            console.log('get token');
+            request.post({
+                url: model.options.authAddr+'/getToken',
+                json: {
+                    agentName: params && params.agentName || model.options.agentName,
+                    agentKey: params && params.agentKey || model.options.agentKey
+                }
+            }, function (err, res, body) {
+                if (err) {
+                    callback(err);
+                } else if (body && body.res > 0){
+                    callback(null, body);
+                } else {
+                    callback(body);
+                }
+            });
+        };
+        model.updateToken = function (callback) {
+            console.log('update token', model.options.authAddr+'/updateToken');
+            if (!model.token) {
+                return model.getToken({}, callback);
+            } else {
+                request.get({
+                    url: model.options.authAddr+'/updateToken',
+                    headers: {
+                        'x-access-token': model.token
+                    },
+                    json: {
+                        agentName: model.options.agentName,
+                        agentKey: model.options.agentKey
+                    }
+                }, function (err, res, body) {
+                    if (err) {
+                        callback(err);
+                    } else if (body && body.res > 0){
+                        callback(null, body);
+                    } else {
+                        callback(body);
+                    }
+                });
+            }
+        };
+        async.retry({times: 5, interval: 5000}, function (cb, ret) {
+            model.getToken({}, cb);
+        }, function (err, res) {
+            if (err) {
+                console.error('init get Token Fail');
+            } else {
+                model.token = res.token;
+                console.log('token get', new Date(), model.token);
+            }
+        });
+        ownedFuns.forEach(function (fun) {
+            //console.log('create function', fun.name);
+            model[fun.name] = function (params, cb) {
+                async.auto({
+                    'waitToken': function (callback) {
+                        async.retry({times: 5, interval: 5000}, function (cbRetry, ret) {
+                            if(model.token) {
+                                cbRetry();
+                            } else {
+                                console.error('Get Token From Memory Error');
+                                cbRetry('Get Token Error');
+                            }
+                        }, callback);
+                    },
+                    'request': ['waitToken', function (ret, callback) {
+                        var reqOptions = {};
+                        var lackField = false;
+                        var paramsClone = _.cloneDeep(params, true);
+                        var url = _.clone(fun.url);
+                        if (~ fun.url.indexOf(':')) {
+                            url = url.replace(/:([A-Za-z_\$][A-Za-z0-9_\$]*)/g, function (match, field) {
+                                if (_.has(paramsClone, field)) {
+                                    paramsClone = _.omit(paramsClone, field);
+                                    //delete params[field]
+                                    return params[field];
+                                } else {
+                                    lackField = true;
+                                    return 'undefined';
+                                }
+                            });
+                            if (lackField) {
+                                 return callback('lack necessary Params');
+                            }
+                        }
+                        reqOptions.url = model.options.authAddr + url;
+                        reqOptions.method = fun.method.toUpperCase();
+                        if (fun.token) {
+                            reqOptions.headers = {
+                                'x-access-token': model.token
+                            };
+                        }
+                        if (fun.method.toUpperCase() == 'GET') {
+                            reqOptions.qs = paramsClone;
+                        } else {
+                            reqOptions.json = paramsClone;
+                        }
+                        request(reqOptions, function (err, res, body) {
+                            if (err) {
+                                callback(err);
+                            } else if (body && body.res > 0){
+                                callback(null, body);
+                            } else {
+                                callback(body);
+                            }
+                        });
+                    }]
+                }, function (err, result) {
+                    cb(err, result.request);
+                });
+            };
+        });
+        setInterval(function(){
+            async.retry({times: 5, interval: 5000}, function (cbRetry, ret) {
+                model.updateToken(cbRetry);
+            }, function (err, res) {
+                    if (err) {
+                        console.error('Update Token Fail');
+                    } else {
+                        model.token = res.token;
+                        console.log('token update', new Date(), model.token);
+                    }
+            });
+        }, options.refresh * 60 * 60 * 1000);
+    } else {
+        console.error('Missing required parameters');
+    }
+    // Make Promise Functions
+    Object.getOwnPropertyNames(model).filter(function (funName) {
+        return _.isFunction(model[funName]);
+    }).forEach(function (funName) {
+         model[funName+'Promise'] = promisify(model[funName], model);
+         //console.log('create function', funName+'Promise')
+    });
+    return model;
+}
+module.exports = {
+    init: init
+};
